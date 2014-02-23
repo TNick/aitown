9733586c932ea7b4b3b75be7cf086463a6e64cd6
Nicu Tofan <nicu.tofan@gmail.com>
2014-02-23 14:43:39 +0200

    Added change part in dejavu

---
+++ aitown-dejavu/dejavu-change.c
@@ -0,0 +1,348 @@
+/* ========================================================================= */
+/* ------------------------------------------------------------------------- */
+/*!
+  \file			dejavu-change.c
+  \date			September 2013
+  \author		TNick
+
+*//*
+
+
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ Please read COPYING and README files in root folder
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+/* ------------------------------------------------------------------------- */
+/* ========================================================================= */
+//
+//
+//
+//
+/*  INCLUDES    ------------------------------------------------------------ */
+
+#include "dejavu-change.h"
+
+#include <aitown/dbg_assert.h>
+#include <aitown/aitown-image.h>
+#include <aitown/utils.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+/*  INCLUDES    ============================================================ */
+//
+//
+//
+//
+/*  DEFINITIONS    --------------------------------------------------------- */
+
+/*  DEFINITIONS    ========================================================= */
+//
+//
+//
+//
+/*  DATA    ---------------------------------------------------------------- */
+
+/*  DATA    ================================================================ */
+//
+//
+//
+//
+/*  FUNCTIONS    ----------------------------------------------------------- */
+
+void aitown_dejavu_change_init (
+        aitown_dejavu_change_t *chg, unsigned width, unsigned height)
+{
+    // as the structure is part of a bigger one and is quite large we refrain from wasting time
+    // memset (chg, 0, sizeof(aitown_dejavu_change_t));
+
+    aitown_dejavu_change_reinit (chg, width, height);
+
+}
+
+
+void aitown_dejavu_change_end (aitown_dejavu_change_t *chg)
+{
+    // as the structure is part of a bigger one and is quite large we refrain from wasting time
+    // memset (chg, 0, sizeof(aitown_dejavu_change_t));
+}
+
+void aitown_dejavu_change_reinit (
+        aitown_dejavu_change_t *chg, unsigned width, unsigned height)
+{
+    // how the grid fits with the window
+    if (width > AITOWN_DEJAVU_CHANGE_COLS) {
+        if (height > AITOWN_DEJAVU_CHANGE_ROWS) {
+            chg->geom = AITOWN_DEJAVU_CHANGE_BOTH_LARGER;
+            chg->pix_h = width / AITOWN_DEJAVU_CHANGE_COLS;
+            chg->pix_v = height / AITOWN_DEJAVU_CHANGE_ROWS;
+            chg->skip_h = width - (AITOWN_DEJAVU_CHANGE_COLS * chg->pix_h);
+            chg->skip_v = height - (AITOWN_DEJAVU_CHANGE_ROWS * chg->pix_v);
+        } else {
+            chg->geom = AITOWN_DEJAVU_CHANGE_VERT_SMALLER;
+            chg->pix_h = width / AITOWN_DEJAVU_CHANGE_COLS;
+            chg->pix_v = 1;
+            chg->skip_h = height - (AITOWN_DEJAVU_CHANGE_ROWS * chg->pix_v);
+            chg->skip_v = AITOWN_DEJAVU_CHANGE_ROWS - height;
+        }
+    } else {
+        if (height > AITOWN_DEJAVU_CHANGE_ROWS) {
+            chg->geom = AITOWN_DEJAVU_CHANGE_HORIZ_SMALLER;
+            chg->pix_h = 1;
+            chg->pix_v = height / AITOWN_DEJAVU_CHANGE_ROWS;
+            chg->skip_h = AITOWN_DEJAVU_CHANGE_COLS - width;
+            chg->skip_v = height - (AITOWN_DEJAVU_CHANGE_ROWS * chg->pix_v);
+        } else {
+            chg->geom = AITOWN_DEJAVU_CHANGE_BOTH_SMALLER;
+            chg->pix_v = 1;
+            chg->pix_h = 1;
+            chg->skip_h = AITOWN_DEJAVU_CHANGE_COLS - width;
+            chg->skip_v = AITOWN_DEJAVU_CHANGE_ROWS - height;
+        }
+    }
+}
+
+static uint32_t average_cell(const uint32_t * p_src, unsigned img_w, unsigned pix_h, unsigned pix_v)
+{
+    int c, r;
+    int cnt = 0;
+    uint64_t sum = 0;
+    const uint32_t * p_row = p_src;
+    for(r=0; r<pix_v; r++) {
+        for(c=0; c<pix_h; c++) {
+            sum += *p_src; p_src++; cnt++;
+        }
+        p_row += img_w;
+        p_src = p_row;
+    }
+    return sum/cnt;
+}
+
+static void aitown_dejavu_change_detect_l (
+        aitown_dejavu_change_t *chg, const aitimage_t * image,
+        const uint32_t * p_cache, uint32_t * p_dest)
+{
+    /** @warning naive implementation */
+
+    uint32_t * p_dif = chg->buf_d;
+    uint32_t grey;
+    uint32_t other_grey;
+    unsigned d_grey_min = UINT_MAX;
+    unsigned d_grey_max = 0;
+    uint64_t d_grey_sum = 0;
+    const uint32_t * p_image = (uint32_t *)AITIMAGE_GET_DATA(image);
+
+    int c, r;
+    unsigned img_w = (chg->pix_h * AITOWN_DEJAVU_CHANGE_COLS) + chg->skip_h;
+    unsigned c_lim = AITOWN_DEJAVU_CHANGE_COLS;
+    unsigned r_lim = AITOWN_DEJAVU_CHANGE_ROWS;
+    for (r=0; r<r_lim; ++r) {
+        for (c=0; c<c_lim; ++c) {
+
+            // compute the offset to first pixel in this cell
+            grey = average_cell(p_cache, img_w, chg->pix_h, chg->pix_v);
+
+            other_grey = *p_cache;
+            *p_dest = grey;
+            grey = (other_grey > grey ? other_grey - grey : grey - other_grey);
+            d_grey_min = (d_grey_min < grey ? d_grey_min : grey);
+            d_grey_max = (d_grey_max > grey ? d_grey_max : grey);
+            d_grey_sum += grey;
+            *p_dif = grey;
+
+            p_image += chg->pix_h;
+            p_dest++; p_cache++; p_dif++;
+        }
+        p_image += chg->skip_h;
+    }
+
+    // compute the average
+    unsigned d_average = (unsigned)(d_grey_sum / AITOWN_DEJAVU_CHANGE_CELLS);
+
+    // and the spread
+    unsigned spread = d_grey_max - d_grey_min;
+
+    // only if the change is large we go on to compute a bounding rectangle
+    if (spread < (AITOWN_DEJAVU_MAX_GREY/4) ) {
+        return;
+    }
+
+    // compute bounding rectangle by selecting only those above average
+    int r_min = INT_MAX;
+    int r_max = -INT_MAX;
+    int c_min = INT_MAX;
+    int c_max = -INT_MAX;
+    p_dif = chg->buf_d;
+    for (c=0; c<c_lim; ++c) {
+        for (r=0; r<r_lim; ++r) {
+            if (*p_dif > d_average) {
+                c_min = (c_min > r ? r : c_min);
+                c_max = (c_max < r ? r : c_max);
+                r_min = (r_min > c ? c : r_min);
+                r_max = (r_max < c ? c : r_max);
+            }
+            p_dif++;
+        }
+    }
+
+    // we need to multiply to get
+    // real coordinates
+    r_min *= chg->pix_v;
+    r_max *= chg->pix_v;
+    c_min *= chg->pix_h;
+    c_max *= chg->pix_h;
+
+    // inform attention module about it
+    d_grey_max -= d_grey_min;
+    d_average -= d_grey_min;
+    if (chg->kb != NULL) {
+        chg->kb (chg->payload, d_average, d_grey_max);
+    }
+
+}
+
+static void aitown_dejavu_change_detect_s (
+        aitown_dejavu_change_t *chg, const aitimage_t * image,
+        const uint32_t * p_cache, uint32_t * p_dest)
+{
+    /** @warning naive implementation */
+
+    uint32_t * p_dif = chg->buf_d;
+    unsigned advance = chg->skip_h;
+    uint32_t rgba;
+    uint32_t grey;
+    uint32_t other_grey;
+    const uint32_t * p_image = (uint32_t *)AITIMAGE_GET_DATA(image);
+    unsigned d_grey_min = UINT_MAX;
+    unsigned d_grey_max = 0;
+    uint64_t d_grey_sum = 0;
+    int i, j;
+
+    
+    // each pixel represents a cell, so there's no averaging required
+    // there is some unused space at the end of each row and
+    // there are some rows that are unused in the bottom portion
+    unsigned i_lim = AITOWN_DEJAVU_CHANGE_COLS - chg->skip_h;
+    unsigned j_lim = AITOWN_DEJAVU_CHANGE_ROWS - chg->skip_v;
+    for (j=0; j<j_lim; ++j) {
+        for (i=0; i<i_lim; ++i) {
+            rgba = *p_image;
+            grey = AITOWN_DEJAVU_CHANGE_TO_GREY(rgba);
+            other_grey = *p_cache;
+            *p_dest = grey;
+            grey = (other_grey > grey ? other_grey - grey : grey - other_grey);
+            d_grey_min = (d_grey_min < grey ? d_grey_min : grey);
+            d_grey_max = (d_grey_max > grey ? d_grey_max : grey);
+            d_grey_sum += grey;
+            *p_dif = grey;
+            p_dest++; p_cache++; p_dif++; p_image++;
+        }
+        p_dest += advance;
+        p_cache += advance;
+        p_dif += advance;
+    }
+
+    // compute the average
+    unsigned d_average = (unsigned)(d_grey_sum / AITOWN_DEJAVU_CHANGE_CELLS);
+
+    // and the spread
+    unsigned spread = d_grey_max - d_grey_min;
+
+    // only if the change is large we go on to compute a bounding rectangle
+    if (spread < (AITOWN_DEJAVU_MAX_GREY/4) ) {
+        return;
+    }
+
+    // compute bounding rectangle by selecting only those above average
+    int r_min = INT_MAX;
+    int r_max = -INT_MAX;
+    int c_min = INT_MAX;
+    int c_max = -INT_MAX;
+    p_dif = chg->buf_d;
+    for (j=0; j<j_lim; ++j) {
+        for (i=0; i<i_lim; ++i) {
+            if (*p_dif > d_average) {
+                c_min = (c_min > i ? i : c_min);
+                c_max = (c_max < i ? i : c_max);
+                r_min = (r_min > j ? j : r_min);
+                r_max = (r_max < j ? j : r_max);
+            }
+            p_dif++;
+        }
+        p_dif += advance;
+    }
+
+    // since each cell is a pixel we don't need to multiply to get
+    // real coordinates
+
+    // inform attention module about it
+    d_grey_max -= d_grey_min;
+    d_average -= d_grey_min;
+    if (chg->kb != NULL) {
+        chg->kb (chg->payload, d_average, d_grey_max);
+    }
+
+}
+
+static void aitown_dejavu_change_detect_hs (
+        aitown_dejavu_change_t *chg, const aitimage_t * image,
+        const uint32_t * p_cache, uint32_t * p_dest)
+{
+    /** @todo implement aitown_dejavu_change_detect_hs() */
+}
+
+static void aitown_dejavu_change_detect_vs (
+        aitown_dejavu_change_t *chg, const aitimage_t * image,
+        const uint32_t * p_cache, uint32_t * p_dest)
+{
+    /** @todo implement aitown_dejavu_change_detect_vs() */
+}
+
+void aitown_dejavu_change_detect (
+        aitown_dejavu_change_t *chg, const aitimage_t * image)
+{
+    DBG_ASSERT (chg != NULL);
+    DBG_ASSERT (image != NULL);
+
+    // switch buffers
+    uint32_t * p_cache;
+    uint32_t * p_dest;
+    if (chg->cache_index == 0) {
+        p_cache = chg->buf_1;
+        p_dest = chg->buf_2;
+        chg->cache_index = 1;
+    } else {
+        p_dest = chg->buf_1;
+        p_cache = chg->buf_2;
+        chg->cache_index = 0;
+    }
+
+    // select the type of processing
+    switch (chg->geom) {
+    case AITOWN_DEJAVU_CHANGE_BOTH_LARGER:
+        aitown_dejavu_change_detect_l (chg, image, p_cache, p_dest);
+        break;
+    case AITOWN_DEJAVU_CHANGE_VERT_SMALLER:
+        aitown_dejavu_change_detect_vs (chg, image, p_cache, p_dest);
+        break;
+    case AITOWN_DEJAVU_CHANGE_HORIZ_SMALLER:
+        aitown_dejavu_change_detect_hs (chg, image, p_cache, p_dest);
+        break;
+    case AITOWN_DEJAVU_CHANGE_BOTH_SMALLER:
+        aitown_dejavu_change_detect_s (chg, image, p_cache, p_dest);
+        break;
+    }
+
+}
+
+
+/*  FUNCTIONS    =========================================================== */
+//
+//
+//
+//
+/* ------------------------------------------------------------------------- */
+/* ========================================================================= */
+
+
---
+++ aitown-dejavu/dejavu-change.h
@@ -0,0 +1,191 @@
+/* ========================================================================= */
+/* ------------------------------------------------------------------------- */
+/*!
+  \file			dejavu-change.h
+  \date			September 2013
+  \author		TNick
+
+  This file defines aitown_dejavu_change_t data structure that groups
+  information related to detecting sudden changes in an image.
+
+  The data uses two buffers that are switching places on each
+  iteration, one being the cache and the other being used to extract data from
+  current image.
+
+  The input is divided into equal portions (the number of rows and
+  columns is configurable at compile time) and an average is computed
+  for each resulting cell. Based on the computed average and minimum-maximum
+  values each point is being evaluated. If the change is high enough
+  a bounding box is computed for virtual pixels that are above a certain
+  treshold.
+
+  A change in input image size induces changes in the size of the
+  cells but not on the number of cells, so no reallocation takes place.
+  If the size of the image is smaller than the number of rows/columns
+  the structure keeps the image in top left corner and zeros the rest.
+
+*//*
+
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ Please read COPYING and README files in root folder
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+/* ------------------------------------------------------------------------- */
+/* ========================================================================= */
+#ifndef AITOWN_dejavu_change_h_INCLUDE
+#define AITOWN_dejavu_change_h_INCLUDE
+//
+//
+//
+//
+/*  INCLUDES    ------------------------------------------------------------ */
+
+#include <aitown/aitown_global.h>
+#include <aitown/aitown-image.h>
+
+#include <inttypes.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*  INCLUDES    ============================================================ */
+//
+//
+//
+//
+/*  DEFINITIONS    --------------------------------------------------------- */
+
+struct _aitimage_t;
+
+// the number of rows and columns that we store
+#ifndef AITOWN_DEJAVU_CHANGE_ROWS
+#define AITOWN_DEJAVU_CHANGE_ROWS 16
+#endif
+#ifndef AITOWN_DEJAVU_CHANGE_COLS
+#define AITOWN_DEJAVU_CHANGE_COLS 16
+#endif
+
+//! the total number of cells
+#define AITOWN_DEJAVU_CHANGE_CELLS \
+    ((AITOWN_DEJAVU_CHANGE_ROWS)*(AITOWN_DEJAVU_CHANGE_COLS))
+
+//! total number of bytes taken by a buffer
+#define AITOWN_DEJAVU_CHANGE_BUFF_SZ ((AITOWN_DEJAVU_CHANGE_CELLS)*4)
+
+//! converting a RGB value to some sort of grey
+#define AITOWN_DEJAVU_CHANGE_TO_GREY(_crt_) \
+        ((AITIMAGE_RGBA_GET_R((_crt_)) * \
+        AITIMAGE_RGBA_GET_G((_crt_)) * \
+        AITIMAGE_RGBA_GET_B((_crt_))) / (1024))
+
+
+//! maximum grey value; minimum is 0
+#define AITOWN_DEJAVU_MAX_GREY (AITOWN_DEJAVU_CHANGE_TO_GREY(0xFFFFFFFF))
+
+
+//! the callback used to inform attention about interesting spots
+typedef void (*aitown_dejavu_change_kb_t) (
+        void * payload,
+        unsigned average,
+        unsigned max
+        );
+
+//! the type of processing based on the geometry of our grid and of the image
+///
+typedef enum _aitown_dejavu_change_geom_t {
+    AITOWN_DEJAVU_CHANGE_BOTH_SMALLER,
+    AITOWN_DEJAVU_CHANGE_HORIZ_SMALLER,
+    AITOWN_DEJAVU_CHANGE_VERT_SMALLER,
+    AITOWN_DEJAVU_CHANGE_BOTH_LARGER
+} aitown_dejavu_change_geom_t;
+
+//! a square area inside an image; attention rectangle is one such area
+typedef struct _aitown_dejavu_change_t {
+    unsigned                pix_h; /**< pixels in each cell in horizontal direction */
+    unsigned                pix_v; /**< pixels in each cell in vertical direction */
+    unsigned                skip_h; /**< number of pixels to set to 0 on horizontal direction */
+    unsigned                skip_v; /**< number of pixels to set to 0 on vertical direction */
+    int                     geom; /**< is the image larger or smaller than our grid? */
+    unsigned                cache_index; /**< tells us the index of the buffer used as cache */
+    aitown_dejavu_change_kb_t   kb; /**< the callback used to inform attention */
+    void *                      payload; /**< will be trasmitted to attention */
+    uint32_t                buf_1[AITOWN_DEJAVU_CHANGE_BUFF_SZ]; /**< the first buffer */
+    uint32_t                buf_2[AITOWN_DEJAVU_CHANGE_BUFF_SZ]; /**< the second buffer */
+    uint32_t                buf_d[AITOWN_DEJAVU_CHANGE_BUFF_SZ]; /**< difference buffer */
+} aitown_dejavu_change_t;
+
+
+/*  DEFINITIONS    ========================================================= */
+//
+//
+//
+//
+/*  DATA    ---------------------------------------------------------------- */
+
+/*  DATA    ================================================================ */
+//
+//
+//
+//
+/*  FUNCTIONS    ----------------------------------------------------------- */
+
+
+//! initialize a dejavu sensor
+///
+/// @param chg      address of the structure to initialise
+/// @param width    width of the input image
+/// @param height   height of the input image
+///
+AITOWN_EXPORT void
+aitown_dejavu_change_init (
+        aitown_dejavu_change_t *chg,
+        unsigned width,
+        unsigned height);
+
+
+//! terminate a dejavu sensor
+///
+/// @param chg   address of the structure to terminate
+///
+AITOWN_EXPORT void
+aitown_dejavu_change_end (
+        aitown_dejavu_change_t *chg);
+
+
+//! informed that the geometry of the image has changed
+///
+/// @param chg      address of the structure
+/// @param width    new width of the input image
+/// @param height   new height of the input image
+///
+AITOWN_EXPORT void
+aitown_dejavu_change_reinit (
+        aitown_dejavu_change_t *chg,
+        unsigned width,
+        unsigned height);
+
+//! called to detect changes from previous runs
+///
+/// @warning the data inside image is never checked and assumed
+/// to be of correct type and size.
+/// 
+/// @param chg      address of the structure
+/// @param image    input data
+///
+AITOWN_EXPORT void
+aitown_dejavu_change_detect (
+        aitown_dejavu_change_t *chg,
+        const struct _aitimage_t * image);
+
+/*  FUNCTIONS    =========================================================== */
+//
+//
+//
+//
+/* ------------------------------------------------------------------------- */
+/* ========================================================================= */
+#ifdef __cplusplus
+}
+#endif
+#endif /* AITOWN_dejavu_change_h_INCLUDE */
